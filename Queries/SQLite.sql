ef3770d2ea603fc76e94ebabbd684defd3334868
DaanvdK09 <van.der.kooij.daan@gmail.com>
2025-05-15 21:54:14 +0200

    Relocate Files

--- Queries/SQLite.sql
+++ Queries/SQLite.sql
@@ -6,7 +6,7 @@ SELECT * FROM user;
 
 -- Add new user
 -- Note: Password is hashed using bcrypt, Hash in Hash.py
---INSERT INTO user (username, password) VALUES ('Test', '$2b$12$ApCjAHEJFcQhuzB9.qYPyeGBeQQyQXRq58G5XBBd1M/pSVJUQHkg');
+-- INSERT INTO user (username, password) VALUES ('Test', '$2b$12$ApCjAHEJFcQhuzB9.qYPyeGBeQQyQXRq58G5XBBd1M/pSVJUQHkg');
 
 -- Remove user
 -- FROM user WHERE (username = 'Test' AND password = '$2b$12$ApCjAHEJFcQhuzB9.qYPyeGBeQQyQXRq58G5XBBd1M/pSVJUQHkg');
