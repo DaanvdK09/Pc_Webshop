bf8f213fcb122c75223c87833533d121dc144f5a
DaanvdK09 <van.der.kooij.daan@gmail.com>
2025-05-19 13:28:56 +0200

    Add pc part Database

---
+++ Queries/pc-parts.sql
@@ -0,0 +1,21 @@
+-- SQLite
+
+-- .tables
+
+SELECT * FROM GPU;
+
+-- Create Table
+-- CREATE TABLE IF NOT EXISTS GPU (
+      -- id INTEGER PRIMARY KEY AUTOINCREMENT,
+      -- name TEXT NOT NULL,
+      -- manufacturer TEXT NOT NULL,
+      -- price REAL NOT NULL,
+      -- memory_size INTEGER NOT NULL,
+      -- memory_type TEXT NOT NULL,
+      -- core_clock REAL NOT NULL,
+      -- boost_clock REAL NOT NULL,
+      -- tdp INTEGER NOT NULL
+-- );
+
+-- Add new
+-- INSERT INTO GPU (name, manufacturer, price, memory_size, memory_type, core_clock, boost_clock, tdp) VALUES ('RTX 5090', 'NVIDIA', 1899.99, 32, 'GDDR7', 1500, 1800, 250);
