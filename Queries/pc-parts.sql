4880335125eb52fa8edae08fd2c4f0a47c7332de
DaanvdK09 <van.der.kooij.daan@gmail.com>
2025-05-19 22:04:38 +0200

    database adjustments

--- Queries/pc-parts.sql
+++ Queries/pc-parts.sql
@@ -18,4 +18,7 @@ SELECT * FROM GPU;
 -- );
 
 -- Add new
--- INSERT INTO GPU (name, manufacturer, price, memory_size, memory_type, core_clock, boost_clock, tdp) VALUES ('RTX 5090', 'NVIDIA', 1899.99, 32, 'GDDR7', 1500, 1800, 250);
+-- INSERT INTO GPU (name, manufacturer, price, memory_size, memory_type, core_clock, boost_clock, tdp) VALUES ('RTX 5090', 'NVIDIA', 1899.99, 32, 'GDDR7', 2017, 2407, 575);
+
+-- Remove
+-- DELETE FROM GPU WHERE (name, manufacturer, price, memory_size, memory_type, core_clock, boost_clock, tdp) = ('RTX 5090', 'NVIDIA', '1899', '32', 'GDDR7', '1500', '1800', '250');
