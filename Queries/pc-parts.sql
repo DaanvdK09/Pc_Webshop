b23a9ef3b01d786e1f08c3872eb6d8db77ed3446
DaanvdK09 <van.der.kooij.daan@gmail.com>
2025-05-20 11:53:29 +0200

    add img_url column

--- Queries/pc-parts.sql
+++ Queries/pc-parts.sql
@@ -14,7 +14,7 @@ SELECT * FROM GPU;
       -- memory_type TEXT NOT NULL,
       -- core_clock REAL NOT NULL,
       -- boost_clock REAL NOT NULL,
-      -- tdp INTEGER NOT NULL
+      -- tdp INTEGER NOT NULL,
 -- );
 
 -- Add new
@@ -22,3 +22,9 @@ SELECT * FROM GPU;
 
 -- Remove
 -- DELETE FROM GPU WHERE (name, manufacturer, price, memory_size, memory_type, core_clock, boost_clock, tdp) = ('RTX 5090', 'NVIDIA', '1899', '32', 'GDDR7', '1500', '1800', '250');
+
+-- Edit
+-- UPDATE GPU SET image_url = https://computercity.com/wp-content/uploads/geforce-rtx-5090-founders-edition-photo-007-transparent-1536x1152.png WHERE id = 1;
+
+-- Add new column
+-- ALTER TABLE GPU ADD COLUMN image_url TEXT;
