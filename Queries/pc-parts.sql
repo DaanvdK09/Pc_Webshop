0220271bbdc72a600ece3fad6ed9471fb9fd267e
DaanvdK09 <van.der.kooij.daan@gmail.com>
2025-05-21 08:50:46 +0200

    edit database

--- Queries/pc-parts.sql
+++ Queries/pc-parts.sql
@@ -17,14 +17,14 @@ SELECT * FROM GPU;
       -- tdp INTEGER NOT NULL,
 -- );
 
--- Add new
+-- Add new item
 -- INSERT INTO GPU (name, manufacturer, price, memory_size, memory_type, core_clock, boost_clock, tdp) VALUES ('RTX 5090', 'NVIDIA', 1899.99, 32, 'GDDR7', 2017, 2407, 575);
 
 -- Remove
 -- DELETE FROM GPU WHERE (name, manufacturer, price, memory_size, memory_type, core_clock, boost_clock, tdp) = ('RTX 5090', 'NVIDIA', '1899', '32', 'GDDR7', '1500', '1800', '250');
 
 -- Edit
--- UPDATE GPU SET image_url = https://computercity.com/wp-content/uploads/geforce-rtx-5090-founders-edition-photo-007-transparent-1536x1152.png WHERE id = 1;
+-- UPDATE GPU SET image_url = 'https://computercity.com/wp-content/uploads/geforce-rtx-5090-founders-edition-photo-007-transparent-1536x1152.png' WHERE id = 1;
 
 -- Add new column
 -- ALTER TABLE GPU ADD COLUMN image_url TEXT;
