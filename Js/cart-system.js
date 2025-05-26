66c6c262ffcd7c249e68c4cd2046b48fca9cc524
Bramblokker <16815@kalsbeek.nl>
2025-05-26 20:42:01 +0200

    a lot of improvements to prebuilt-pc page and made a start on the shopping cart system

---
+++ Js/cart-system.js
@@ -0,0 +1,38 @@
+// Hier is Bram nog mee bezig, maar ik moet nog ffe bedenken hoe ik de specs erbij ga zetten
+const products = [{
+    image: '../Foto/budget-prebuilt-gaming-pc-white.png',
+    name: 'Budget Prebuilt Gaming PC',
+    priceCents: 89999
+}, {
+    image: '../Foto/budget-prebuilt-gaming-pc-black.png',
+    name: 'Budget Prebuilt Gaming PC Black',
+    priceCents: 79999
+}, {
+    image: '../Foto/prebuild-pc-mr-white.jpg',
+    name: 'Midrange Gaming PC White',
+    priceCents: 139999
+}, {
+    image: '../Foto/prebuilt-pc-mr-black.jpg',
+    name: 'Midrange Gaming PC Black',
+    priceCents: 139999
+}, {
+    image: '../Foto/high-end-prebuilt-gaming-pc-white.png',
+    name: 'High-End Prebuilt Gaming PC White',
+    priceCents: 289999
+}, {
+    image: '../Foto/high-end-prebuilt-gaming-pc-black.png',
+    name: 'High-End Prebuilt Gaming PC Black',
+    priceCents: 279999
+}, {
+    image: '../Foto/ultra-high-end-prebuilt-gaming-pc-white.png',
+    name: 'Ultra High-end Gaming PC White',
+    priceCents: 439999
+}, {
+    image: '../Foto/ultra-high-end-prebuilt-gaming-pc-black.png',
+    name: 'Ultra High-end Gaming PC Black',
+    priceCents: 429999
+}]
+
+products.forEach((product) => {
+    const html = ``
+});
